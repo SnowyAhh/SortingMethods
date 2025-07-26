77fc034f87ddcc714a0ea3cbcf82bda3e7574b24
SnowyAhh <sylviahuynh17@gmail.com>
2025-07-26 20:44:28 +1000

    Added unit tests for selection sort

--- src/unitTests/selectionSortTest.py
+++ src/unitTests/selectionSortTest.py
@@ -3,8 +3,27 @@ from src.sortMethods import selectionSort
 
 # Use python -m unittest .\src\unitTests\selectionSortTest.py to run
 class TestSelectionSort(unittest.TestCase):
+    # Test for empty array
     def test_sort_null(self):
-        self.assertEqual([], [])
+        self.assertEqual(selectionSort.sort([]), [])
 
-if __name__ == '__main__':
-    unittest.main()
+    # Test for array with single element
+    def test_sort_single(self):
+        self.assertEqual(selectionSort.sort([0]), [0])
+        self.assertEqual(selectionSort.sort([-100]), [-100])
+
+    # Test for array with multiple elements (no dupes)
+    def test_sort_no_dupes(self):
+        self.assertEqual(selectionSort.sort([1,3,4,2,5]), [1,2,3,4,5])
+        self.assertEqual(selectionSort.sort([-10,54,20,4,6111,-53,0,12]), 
+                                            [-53,-10,0,4,12,20,54,6111])
+        self.assertEqual(selectionSort.sort([5,4,3,2,1]), [1,2,3,4,5])
+        self.assertEqual(selectionSort.sort([-1,-2,-3,-4,-5]), [-5,-4,-3,-2,-1])
+        self.assertEqual(selectionSort.sort([1,3]), [1,3])
+
+    # Test for array with duplicate elements
+    def test_sort_dupes(self):
+        self.assertEqual(selectionSort.sort([1,1,1]), [1,1,1])
+        self.assertEqual(selectionSort.sort([1,2,2,1]), [1,1,2,2])
+        self.assertEqual(selectionSort.sort([5,3,1,5,-2,-6,5]), 
+                                            [-6,-2,1,3,5,5,5])
