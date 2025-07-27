782317087f5fb1d5f3df596971db8559e1ed7f4b
SnowyAhh <sylviahuynh17@gmail.com>
2025-07-27 19:38:59 +1000

    Added unit testing for bubble sort - the same tests for selection sort

---
+++ src/unitTests/bubbleSortTest.py
@@ -0,0 +1,42 @@
+import unittest
+from src.sortMethods import bubbleSort
+
+# Use python -m unittest .\src\unitTests\bubbleSortTest.py to run
+class TestBubbleSort(unittest.TestCase):
+    # Test for arrays holding non-integers (testing util file)
+    def test_non_int(self):
+        self.assertEqual(bubbleSort.sort(['a', False, 'b']), 
+                                            ['a', False, 'b'])
+
+    # Test for arrays holding non-integers and integers (testing util file)
+    def test_non_int_and_int(self):
+        self.assertEqual(bubbleSort.sort([1,'a',False,'b']), 
+                                            [1,'a',False,'b'])
+        self.assertEqual(bubbleSort.sort(['a',False,1,'b']), 
+                                            ['a',False,1,'b'])
+
+    # Tests for selection sort
+    # Test for empty array
+    def test_sort_null(self):
+        self.assertEqual(bubbleSort.sort([]), [])
+
+    # Test for array with single element
+    def test_sort_single(self):
+        self.assertEqual(bubbleSort.sort([0]), [0])
+        self.assertEqual(bubbleSort.sort([-100]), [-100])
+
+    # Test for array with multiple elements (no dupes)
+    def test_sort_no_dupes(self):
+        self.assertEqual(bubbleSort.sort([1,3,4,2,5]), [1,2,3,4,5])
+        self.assertEqual(bubbleSort.sort([-10,54,20,4,6111,-53,0,12]), 
+                                            [-53,-10,0,4,12,20,54,6111])
+        self.assertEqual(bubbleSort.sort([5,4,3,2,1]), [1,2,3,4,5])
+        self.assertEqual(bubbleSort.sort([-1,-2,-3,-4,-5]), [-5,-4,-3,-2,-1])
+        self.assertEqual(bubbleSort.sort([1,3]), [1,3])
+
+    # Test for array with duplicate elements
+    def test_sort_dupes(self):
+        self.assertEqual(bubbleSort.sort([1,1,1]), [1,1,1])
+        self.assertEqual(bubbleSort.sort([1,2,2,1]), [1,1,2,2])
+        self.assertEqual(bubbleSort.sort([5,3,1,5,-2,-6,5]), 
+                                            [-6,-2,1,3,5,5,5])
